2c748f99291caa0e18ea303fb9376b99c7bad0fa
Antoine <pollet.antoine.alexis@gmail.com>
2023-04-11 19:07:26 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	setup() {},
+});
+</script>
+
+<template>
+	<div class="h-100vh">RepairNow</div>
+</template>
+
+<style scoped></style>
