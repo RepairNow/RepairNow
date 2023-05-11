970089ab93019fced36d02e71cf55048cf212c4b
Michael De Madet <michaeldemadet@gmail.com>
2023-05-11 12:41:23 +0200

    add swagger

--- apps/back/api-gateway/src/main.ts
+++ apps/back/api-gateway/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
   const configService = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Repair Now API Doc')
+    .setDescription('The Repair Now API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(configService.get('API_GATEWAY_PORT'), '0.0.0.0', function () {
     console.log('Hey ya ya ya ! ApiGateway is listening on port ' + configService.get('API_GATEWAY_PORT'));
   });
