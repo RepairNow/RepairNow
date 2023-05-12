70b4717f2b436b9461e97b755e4100f19e2a8697
NilsMillot <compte.mxs@hotmail.fr>
2023-05-12 18:55:14 +0200

    fix unit test

--- apps/back/api-gateway/src/api-gateway.controller.spec.ts
+++ apps/back/api-gateway/src/api-gateway.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('ApiGatewayController', () => {
   let apiGatewayController: ApiGatewayController;
@@ -16,6 +17,7 @@ describe('ApiGatewayController', () => {
             callAuth: jest.fn(),
           },
         },
+        JwtService,
       ],
     }).compile();
 
