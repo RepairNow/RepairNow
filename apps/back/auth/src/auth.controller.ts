8f95b322b473be6bef44355b939a6db301d0fc7c
Michael De Madet <michaeldemadet@gmail.com>
2023-05-18 18:19:27 +0200

    test

--- apps/back/auth/src/auth.controller.ts
+++ apps/back/auth/src/auth.controller.ts
@@ -20,7 +20,7 @@ export class AuthController {
 
   @Get()
   getHello(): string {
-    console.log('getHellsery');
+    console.log('getHellseertry');
     return this.authService.getHello();
   }
 
