d076512a91082933ab9dbf4653c4dc77e83279f6
Antoine <pollet.antoine.alexis@gmail.com>
2023-06-14 14:50:48 +0200

    Init font awesome

--- apps/front/src/main.ts
+++ apps/front/src/main.ts
@@ -8,6 +8,18 @@ import i18n from "./plugins/i18n";
 import "@/main.css";
 import App from "./App.vue";
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
 const pinia = createPinia();
 
 const app = createApp(App);
@@ -17,7 +29,8 @@ app.use(pinia);
 app.use(router);
 app.use(vuetify);
 app.use(trackerFront, {
-	APP_ID: "front-app",
-	//router
+    APP_ID: "front-app",
+    //router
 });
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount("#app");
