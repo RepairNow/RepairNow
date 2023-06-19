587daa63c964bdd4ee555c54ae71a0b1fc970910
Michael De Madet <michaeldemadet@gmail.com>
2023-06-19 16:27:13 +0200

    add auth guards

--- apps/back/api-gateway/src/job/job.controller.ts
+++ apps/back/api-gateway/src/job/job.controller.ts
@@ -1,30 +1,36 @@
-import { Body, Controller, Delete, Get, Inject, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('jobs')
 export class JobController {
     constructor(@Inject('JOB_SERVICE') private jobClient: ClientProxy) { }
 
+    @UseGuards(AuthGuard)
     @Post()
     create(@Body() createJobDto: any) {
         return this.jobClient.send({ cmd: 'createJob' }, createJobDto);
     }
 
+    @UseGuards(AuthGuard)
     @Get()
     findAll() {
         return this.jobClient.send({ cmd: 'findAllJobs' }, '');
     }
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.jobClient.send({ cmd: 'findOneJob' }, id);
     }
 
+    @UseGuards(AuthGuard)
     @Patch()
     update(@Body() updateJobDto: any) {
         return this.jobClient.send({ cmd: 'updateJob' }, updateJobDto);
     }
 
+    @UseGuards(AuthGuard)
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.jobClient.send({ cmd: 'removeJob' }, id);
--- apps/back/api-gateway/src/mission/mission.controller.ts
+++ apps/back/api-gateway/src/mission/mission.controller.ts
@@ -1,30 +1,36 @@
-import { Body, Controller, Delete, Get, Inject, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('missions')
 export class MissionController {
     constructor(@Inject('MISSION_SERVICE') private missionClient: ClientProxy) { }
 
+    @UseGuards(AuthGuard)
     @Post()
     create(@Body() createMissionDto: any) {
         return this.missionClient.send({ cmd: 'createMission' }, createMissionDto);
     }
 
+    @UseGuards(AuthGuard)
     @Get()
     findAll() {
         return this.missionClient.send({ cmd: 'findAllMission' }, '');
     }
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.missionClient.send({ cmd: 'findOneMission' }, id);
     }
 
+    @UseGuards(AuthGuard)
     @Patch()
     update(@Body() updateMissionDto: any) {
         return this.missionClient.send({ cmd: 'updateMission' }, updateMissionDto);
     }
 
+    @UseGuards(AuthGuard)
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.missionClient.send({ cmd: 'removeMission' }, id);
