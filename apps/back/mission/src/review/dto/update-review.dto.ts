60768474b9488f560f6da77a374939faed21d54e
Michael De Madet <michaeldemadet@gmail.com>
2023-06-20 16:37:02 +0200

    crud review

--- apps/back/mission/src/review/dto/update-review.dto.ts
+++ apps/back/mission/src/review/dto/update-review.dto.ts
@@ -1,6 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateReviewDto } from './create-review.dto';
+import { IsNotEmpty, IsUUID } from "class-validator";
+
 
 export class UpdateReviewDto extends PartialType(CreateReviewDto) {
-  id: number;
+  @IsNotEmpty()
+  @IsUUID()
+  id: string;
 }
