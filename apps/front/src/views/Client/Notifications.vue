103d6dd022f2f31c137bd9f5d8f182ae311aa52b
Antoine lin <lin.antoine.pro@gmail.com>
2023-06-20 21:09:29 +0200

    notifications

--- apps/front/src/views/Client/Notifications.vue
+++ apps/front/src/views/Client/Notifications.vue
@@ -1,8 +1,28 @@
 <template>
-
+    <div class="tw-flex tw-flex-col tw-gap-5 tw-m-5 xl:tw-mx-64">
+        <div class="tw-font-bold tw-text-2xl lg:tw-text-4xl">
+            Mes notifications
+        </div>
+        <div class="tw-flex tw-flex-col tw-gap-4 tw-my-4">
+            <notification
+                    v-for="notification in notifications"
+                    :notification="notification"
+            />
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+import Notification from "@/components/notification/notification.vue";
+import {ref} from "vue";
+
+const notifications = ref<Notification[]>([
+    {
+        title: 'notification title',
+        content: 'notification content',
+        created_at: '12/02/10',
+    },
+])
 </script>
 
 <style scoped>
