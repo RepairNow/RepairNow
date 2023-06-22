03801bb203059a66a8988de0c25f16b1367b4b6f
Antoine lin <lin.antoine.pro@gmail.com>
2023-06-23 01:27:59 +0200

    update admin menu

--- apps/front/src/views/Admin/Admin.vue
+++ apps/front/src/views/Admin/Admin.vue
@@ -1,14 +1,25 @@
 <template>
-    <div class="tw-flex tw-h-screen tw-w-screen tw-py-16 sm:tw-pb-0">
-        <mobile-navigation/>
-        <mobile-drawer/>
+    <div v-if="!isSizeSM" class="tw-flex tw-h-screen tw-w-screen tw-py-16 sm:tw-pb-0">
         <header-navigation/>
         <div class="tw-bg-green-100 tw-h-full tw-w-80 tw-hidden sm:tw-flex">
             <sidebar-navigation/>
         </div>
         <div class="tw-bg-green-100 tw-h-full tw-w-full">
+            {{isSizeSM}}
             <router-view />
         </div>
+    </div>
+    <div
+        v-else
+        class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-screen tw-gap-10"
+    >
+        <img src="/undraw_no_data_re_kwbl.svg" alt="" class="tw-w-1/3"/>
+        <p class="tw-text-xl tw-px-10 tw-text-center tw-font-bold">
+            Admin interface is not available in mobile device
+        </p>
+        <router-link :to="{name: 'home'}" class="tw-text-primary tw-underline">
+            Go back to homepage
+        </router-link>
     </div>
 </template>
 
@@ -17,6 +28,11 @@ import MobileDrawer from "@/components/structure/admin/admin-mobile-drawer.vue";
 import MobileNavigation from "@/components/structure/admin/admin-mobile-navigation.vue";
 import HeaderNavigation from "@/components/structure/admin/admin-header-navigation.vue";
 import SidebarNavigation from "@/components/structure/admin/admin-sidebar-navigation.vue";
+import { useScreenSize } from "@/stores/screen-size";
+import {storeToRefs} from "pinia";
+
+const screenSize = useScreenSize()
+const { isSizeSM } = storeToRefs(screenSize)
 </script>
 
 <style scoped>
