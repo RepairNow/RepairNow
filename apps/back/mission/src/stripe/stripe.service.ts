1186b89f8d0c616a349bb509f298b2e01d4aed22
Michael De Madet <michaeldemadet@gmail.com>
2023-06-24 20:14:44 +0200

    add stripe payment when estimate is accepted (#82)

---
+++ apps/back/mission/src/stripe/stripe.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class StripeService {
+    private stripe;
+
+    constructor(private configService: ConfigService) {
+        const stripeSecretKey = this.configService.get('STRIPE_SECRET_KEY');
+        this.stripe = new Stripe(stripeSecretKey, {
+            apiVersion: '2022-11-15',
+        });
+    }
+
+    async createCheckoutSession(price: number): Promise<string> {
+        const session = await this.stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'eur',
+                        unit_amount: Math.round(price * 100),
+                        product_data: {
+                            name: 'Service Repair Now',
+                        },
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            success_url: this.configService.get('STRIPE_CHECKOUT_SUCESS_URL'),
+            cancel_url: this.configService.get('STRIPE_CHECKOUT_CANCEL_URL')
+        });
+
+        return session.url;
+    }
+}
