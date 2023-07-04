5cc192a9d8b70fbd007b87273c607fc7bfafc2a4
Michael De Madet <michaeldemadet@gmail.com>
2023-07-04 09:52:04 +0200

    first map

--- apps/front/src/views/Client/GeoLocation.vue
+++ apps/front/src/views/Client/GeoLocation.vue
@@ -1,8 +1,16 @@
 <template>
-
+    <l-map ref="map" :zoom="2" :center="[47.41322, -1.219482]" style="width: 1000px;">
+        <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+        style="width: 1000px;"
+        ></l-tile-layer>
+        <l-control-zoom position="bottomright" zoom-in-text="*" zoom-out-text="/" />
+    </l-map>
 </template>
-
 <script setup lang="ts">
+import { LControlZoom, LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 </script>
 
 <style scoped>
