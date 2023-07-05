49426c901d9a50e8df617bf0a774f41b40d2c5cc
Antoine <pollet.antoine.alexis@gmail.com>
2023-07-05 18:57:27 +0200

    Send invoice when devis is paid

--- apps/back/mission/src/app.module.ts
+++ apps/back/mission/src/app.module.ts
@@ -9,13 +9,31 @@ import { GeolocationModule } from './geolocation/geolocation.module';
 import { ValidationCodeService } from './validation.code/validation.code.service';
 import { ValidationCodeController } from './validation.code/validation.code.controller';
 import { ValidationCodeModule } from './validation.code/validation.code.module';
-
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MissionModule, ReviewModule, AnnouncementsModule, EstimatesModule, GeolocationModule, ValidationCodeModule],
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAILER_SERVER'),
+          port: 465,
+          secure: true,
+          auth: {
+            user: configService.get('MAILER_FROM'),
+            pass: configService.get('MAILER_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>'
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    MissionModule, ReviewModule, AnnouncementsModule, EstimatesModule, GeolocationModule, ValidationCodeModule, MailerModule],
   providers: [PrismaService, ValidationCodeService],
   controllers: [ValidationCodeController]
 
