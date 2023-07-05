351e8e047a013513a777ca0273a54cc1575e9c52
Antoine <pollet.antoine.alexis@gmail.com>
2023-07-05 13:32:58 +0200

    Back mailing

--- apps/back/auth/src/auth.module.ts
+++ apps/back/auth/src/auth.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaService } from '@repairnow/prisma';
 import { UsersModule } from './users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { MailerModule, MailerService } from '@nestjs-modules/mailer';
 import SmsController from './sms/sms.controller';
 import SmsService from './sms/sms.service';
 
@@ -28,6 +29,23 @@ import SmsService from './sms/sms.service';
       // TODO: I don't know if the secret is being read from the env file. Check this.
       secret: `${process.cwd()}/env/${process.env.NODE_ENV}.env.JWT_SECRET`,
       signOptions: { expiresIn: '120s' },
+    }),
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAILER_SERVER'),
+          port: 465,
+          secure: true,
+          auth: {
+            user: configService.get('MAILER_FROM'),
+            pass: configService.get('MAILER_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>'
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController, SmsController],
