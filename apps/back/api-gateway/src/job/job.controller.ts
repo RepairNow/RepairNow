6f490a99b4164414ca5b9ee3eaaab7b7392d022c
NilsMillot <compte.mxs@hotmail.fr>
2023-07-05 17:30:31 +0200

    protect back to create, delete and patch jobs

--- apps/back/api-gateway/src/job/job.controller.ts
+++ apps/back/api-gateway/src/job/job.controller.ts
@@ -1,34 +1,46 @@
-import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { AuthGuard } from 'src/guards/auth.guard';
+import PermissionGuard from 'src/guards/permissionGuard';
 
 @Controller('jobs')
-@UseGuards(AuthGuard)
 export class JobController {
-    constructor(@Inject('JOB_SERVICE') private jobClient: ClientProxy) { }
+  constructor(@Inject('JOB_SERVICE') private jobClient: ClientProxy) {}
 
-    @Post()
-    create(@Body() createJobDto: any) {
-        return this.jobClient.send({ cmd: 'createJob' }, createJobDto);
-    }
+  @UseGuards(PermissionGuard('ADMIN'))
+  @Post()
+  create(@Body() createJobDto: any) {
+    return this.jobClient.send({ cmd: 'createJob' }, createJobDto);
+  }
 
-    @Get()
-    findAll() {
-        return this.jobClient.send({ cmd: 'findAllJobs' }, '');
-    }
+  @Get()
+  findAll() {
+    return this.jobClient.send({ cmd: 'findAllJobs' }, '');
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.jobClient.send({ cmd: 'findOneJob' }, id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.jobClient.send({ cmd: 'findOneJob' }, id);
+  }
 
-    @Patch()
-    update(@Body() updateJobDto: any) {
-        return this.jobClient.send({ cmd: 'updateJob' }, updateJobDto);
-    }
+  @UseGuards(PermissionGuard('ADMIN'))
+  @Patch()
+  update(@Body() updateJobDto: any) {
+    return this.jobClient.send({ cmd: 'updateJob' }, updateJobDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.jobClient.send({ cmd: 'removeJob' }, id);
-    }
+  @UseGuards(PermissionGuard('ADMIN'))
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.jobClient.send({ cmd: 'removeJob' }, id);
+  }
 }
