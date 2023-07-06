49cbd2e646d6262085c6f08733fd93f3aec4a100
Antoine <pollet.antoine.alexis@gmail.com>
2023-07-06 18:31:57 +0200

    file size updated to 10MB

--- apps/back/api-gateway/src/api-gateway.controller.ts
+++ apps/back/api-gateway/src/api-gateway.controller.ts
@@ -82,7 +82,7 @@ export class ApiGatewayController {
   @UseGuards(AuthGuard)
   @UseInterceptors(FileInterceptor('file'))
   updateAvatar(
-    @UploadedFile(new ParseFilePipe({ validators: [new MaxFileSizeValidator({ maxSize: 1000000 })]})) file: Express.Multer.File,
+    @UploadedFile(new ParseFilePipe({ validators: [new MaxFileSizeValidator({ maxSize: 10000000 })]})) file: Express.Multer.File,
     @Request() request,
   ): Observable<any> {
     const { user } = request;
