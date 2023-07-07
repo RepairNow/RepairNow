9d43defdcfaacb6e13386ed3e0f89957df5b0f51
Antoine lin <lin.antoine.pro@gmail.com>
2023-07-07 11:36:24 +0200

    add error for code

--- apps/front/src/components/modal/form/missions/mission-validation-modal.vue
+++ apps/front/src/components/modal/form/missions/mission-validation-modal.vue
@@ -14,11 +14,16 @@
 		<v-card class="tw-w-2/3 tw-mx-auto tw-p-4 rounded-lg">
 			<div class="tw-flex">
 				<v-spacer></v-spacer>
-				<v-btn icon="mdi-close" color="none" @click="dialog = false" />
+				<v-btn icon="mdi-close" color="none" @click="() => {
+                        dialog = false;
+                        error = ''
+				    }"
+                />
 			</div>
             <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-text-center">
                 <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-text-center">
                     <div class="tw-font-bold tw-text-xl tw-mb-4">Valider la mission</div>
+                    <p class="tw-text-red-400">{{error}}</p>
                     <v-text-field type="number" class="tw-w-32" v-model="code"/>
                     <p>Pour valider la mission, veuillez renseigner le code de validation partager par votre prestataire</p>
                     <v-btn
@@ -63,13 +68,15 @@ const { isSizeLG } = storeToRefs(screenSizeStore);
 
 const router = useRouter();
 
+const error = ref('')
 const validateFourDigitCode = async () => {
 	try {
 		await validateCode(props.mission.id, parseInt(code.value))
 		dialog.value = false;
         await getAnnouncement(route.params.id.toString())
-    } catch (error) {
-		console.error(error);
+    } catch (e: any) {
+        error.value = e.response.data.message
+		console.error(e);
 	}
 }
 </script>
